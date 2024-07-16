ededb71d578e3f6a6764f2a9f2a879b60e74bf1d
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-16 23:57:08 +0530

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -59,7 +59,7 @@ export class AuthService {
   async signToken(userId: number, email: string): Promise<{ access_token: string }> {
     const payload = { sub: userId, email };
     const secret = this.config.get('JWT_SECRET');
-    const token = await this.jwt.signAsync(payload, { expiresIn: '15m', secret });
+    const token = await this.jwt.signAsync(payload, { expiresIn: '1D', secret });
     return {
       access_token: token,
     };
