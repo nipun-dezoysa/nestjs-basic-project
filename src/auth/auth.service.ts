d363f149620aadf1399080cf538d58e20f6aba51
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-16 12:02:28 +0530

    prisma create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import { User,Bookmark } from '@prisma/client';
-@Injectable({})
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+@Injectable()
 export class AuthService {
-  signup() {
-    return 'signup';
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password); //password hashing
+    try {
+      //prisma add new row to user
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName: '',
+          lastName: '',
+        },
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
+      delete user.hash;
+      return user;
+    } catch (e) {
+      if(e instanceof PrismaClientKnownRequestError){
+        //prisma error handling
+        if(e.code === 'P2002'){
+          throw new Error('Email already exists');
+        }
+      }
+      throw e;
+    }
   }
 
   signin() {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({whitelist: true})); //whitelist: only allow properties that are defined in the DTO
   await app.listen(3333);
 }
 bootstrap();
