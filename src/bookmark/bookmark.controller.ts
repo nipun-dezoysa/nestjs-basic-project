4c915d34495e32aed61319cd02cadefa451ad104
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-21 21:04:02 +0530

    bookmark controller

---
+++ src/bookmark/bookmark.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { BookmarkService } from './bookmark.service';
+import { GetUser } from 'src/auth/decorator';
+
+@UseGuards(JwtGuard)
+@Controller('bookmarks')
+export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+  @Get()
+  getBookmarks(@GetUser('id') userId: number) {}
+  @Get(':id')
+  getBookmarkById(@GetUser('id') userId: number,@Param('id',ParseIntPipe)bookmarkId :number) {}
+  @Post()
+  createBookmark(@GetUser('id') userId: number) {}
+  @Patch()
+  editBookmarkById(@GetUser('id') userId: number) {}
+  @Delete()
+  deleteBookmarkById(@GetUser('id') userId: number) {}
+}
